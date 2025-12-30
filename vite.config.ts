678135d4896183670cbc8205e7cdf53c14b66ddb
laurenawilkinson <lauren.a.wilkinson@outlook.com>
2025-12-30 20:29:38 +0000

    build: move index.html and adjust vite config so dist appears in root dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
   plugins: [tailwindcss()],
 });
